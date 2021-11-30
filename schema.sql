8458016ab26741126be3fcaf49c4f7eb46c1532d
Ruchi Mathur <ruchim3@gmail.com>
2021-11-30 12:53:30 -0800

    Adding queries

--- schema.sql
+++ schema.sql
@@ -23,7 +23,7 @@ dept_no VARCHAR(4) NOT NULL,
     to_date DATE NOT NULL,
 FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-    PRIMARY KEY (emp_no, dept_no)
+    PRIMARY KEY (emp_no)
 );
 
 CREATE TABLE salaries (
@@ -50,8 +50,7 @@ CREATE TABLE titles (
   from_date DATE NOT NULL,
   to_date DATE NOT NULL,
   FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  PRIMARY KEY (emp_no)
-);
+  );
 
 SELECT *
 FROM departments
